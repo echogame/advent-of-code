c8b2e5156a16c6546fe3084934f2b6e36b42fe3d
Yin Li <echogame@users.noreply.github.com>
2021-09-06 11:18:05 -0700

    Adds more sample input

--- 2020/day-07/input.js
+++ 2020/day-07/input.js
@@ -8,6 +8,13 @@ dark olive bags contain 3 faded blue bags, 4 dotted black bags.
 vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
 faded blue bags contain no other bags.
 dotted black bags contain no other bags.`,
+  SAMPLE_RULES2: `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`,
   RULES: `vibrant bronze bags contain 3 dim olive bags.
 shiny teal bags contain 1 posh green bag, 5 pale indigo bags, 1 mirrored purple bag.
 striped aqua bags contain 5 bright orange bags.
